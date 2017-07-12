06dbf00be1d334e686446f6fba48b744290737a4
Gabriel <gabriel.wrowe@gmail.com>
2017-07-12 11:23:43 -0400

    update: navbar with react-materialize

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,59 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Navbar, NavItem } from 'react-materialize'
 
 
 
-const Navbar = (props) => (
-  <div style={{backgroundColor: "#e3f2fd"}}>
-    <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-      <Link className="navbar-brand" to="/calculator">Navbar w/ text</Link>
-      <div className="collapse navbar-collapse" id="navbarText">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/calculator">Calculator <span className="sr-only"></span></Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-        </ul>
-        <span className="navbar-text">
-          Figure out what you need
-        </span>
-      </div>
-    </nav>
-  </div>
+const Nav = (props) => (
+  <Navbar style={{backgroundColor: "lightblue"}} brand='Figure out what you need' left>
+    <NavItem><Link to='/calculator'>Calculator</Link></NavItem>
+    <NavItem><Link to='/about'>About</Link></NavItem>
+  </Navbar>
 )
 
-export default Navbar
-
-
-
-
-
-// <nav className="navbar navbar-light" style={{backgroundColor:"#e3f2fd"}}>
-//       <Link className="navbar-brand" to="/calculator">Retirement Calculator!</Link>
-//         <div className="navbar-nav">
-//           <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
-//           <a className="nav-item nav-link" href="#">Features</a>
-//           <a className="nav-item nav-link" href="#">Pricing</a>
-//         </div>
-//     </nav>
-
-
-
-// <header>
-//       <h1>Investment Calculator!</h1>
-//       <h2>Figure out what you'll need.</h2>
-//       <nav>
-//         <ul>
-//           <li><Link to="/calculator">Calculator</Link></li>
-//           <li><Link to="/about">About</Link></li>
-//         </ul>
-//       </nav>
-//     </header>
-
-
-
-
-
+export default Nav
 
