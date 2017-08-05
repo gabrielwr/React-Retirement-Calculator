d20a18dc4873d78423480214382cbeb43497f88d
Gabriel <gabriel.wrowe@gmail.com>
2017-08-05 10:15:50 -0400

    feat: barChart working

---
+++ client/components/BarChart.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ResponsiveContainer, XAxis, YAxis, BarChart, Bar, Legend, Tooltip } from 'recharts'
+
+import { formatMoney } from '../utils/formatMoney'
+
+export default class DynamicChart extends React.Component {
+  constructor(){
+    super()
+  }
+
+  render() {
+    const barArr = [{
+      retireAmt: this.props.retireAmt,
+      endAmt: this.props.finalAmount
+    }]
+
+    return (
+      <ResponsiveContainer width={'15%'} height={'90%'}>
+        <BarChart
+          data={ barArr }
+          margin={{top: 5, right: 30, left: 20, bottom: 5}}
+        >
+        <XAxis dataKey="name"/>
+        <YAxis
+            tickFormatter={ money => '$' + formatMoney(+money, 0)}
+        />
+        {/* <CartesianGrid strokeDasharray="3 3"/> */}
+        <Tooltip
+          labelFormatter={(amt) => (`Retire Amount ${amt}`)}
+          formatter={(finalAmt) => (`$${formatMoney(+finalAmt, 0)}`)}
+        />
+        <Legend />
+        <Bar dataKey="retireAmt" fill="#7FDBFF" />
+        <Bar dataKey="endAmt" fill="#39CCCC" />
+        </BarChart>
+      </ResponsiveContainer>
+    )
+  }
+}
+
+// BarChart style
+
