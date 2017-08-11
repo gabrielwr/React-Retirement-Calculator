8876da5a0c8032f49c4d250a66935b4c58b6049e
Gabriel <gabriel.wrowe@gmail.com>
2017-08-11 16:09:28 -0400

    feat: update combinereducers require statements

--- client/reducers/index.jsx
+++ client/reducers/index.jsx
@@ -1,22 +1,31 @@
 import { combineReducers } from 'redux';
 
+import currentAge from './currentAge';
+import currentSavings from './currentSavings';
+import finalAmount from './finalAmt';
+import graphData from './graphData';
+import lifespan from './lifespan';
+import marketReturn from './marketReturn';
+import retireAge from './retireAge';
+import retireAmt from './retireAmt';
+import retireSpending from './retireSpending';
+import salary from './salary';
+import salaryIncrease from './salaryIncrease';
+import savings from './savings';
 
-//should I use import here instead?
-//how is this working with require since I'm not
-// module.export-ing?
 const rootReducer = combineReducers({
-  currentAge: require( './currentAge' ).default,
-  currentSavings: require( './currentSavings' ).default,
-  finalAmount: require( './finalAmt' ).default,
-  graphData: require( './graphData' ).default,
-  lifespan: require( './lifespan' ).default,
-  marketReturn: require( './marketReturn' ).default,
-  retireAge: require( './retireAge' ).default,
-  retireAmt: require( './retireAmt' ).default,
-  retireSpending: require( './retireSpending' ).default,
-  salary: require( './salary' ).default,
-  salaryIncrease: require( './salaryIncrease' ).default,
-  savings: require( './savings' ).default,
+  currentAge,
+  currentSavings,
+  finalAmount,
+  graphData,
+  lifespan,
+  marketReturn,
+  retireAge,
+  retireAmt,
+  retireSpending,
+  salary,
+  salaryIncrease,
+  savings
 })
 
 export default rootReducer;
