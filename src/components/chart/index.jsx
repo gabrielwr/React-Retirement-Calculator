ab53c8ce9c3f0350aa7ab353e1573fe11987cfc3
Gabriel <gabriel.wrowe@gmail.com>
2018-04-04 21:04:31 -0500

    removes extraneous comments and relative path-ing from chart component

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -3,12 +3,11 @@ import { connect } from 'react-redux';
 
 import '../../css/index.css';
 
-import LineChart from '../../elements/line-chart/index.jsx';
-import BarChart from '../../elements/bar-chart/index.jsx';
+import LineChart from '../../elements/line-chart/index';
+import BarChart from '../../elements/bar-chart/index';
 
 class Chart extends Component {
   render() {
-    console.log('STATE IN CHART', this.props)
     const { finalAmount, graphData, retireAmt } = this.props;
     return (
       <div className="chartContainer">
