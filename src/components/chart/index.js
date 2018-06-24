8304349b00ea870a8e1b2da0d258a035df72ca56
Gabriel <Gabriel.wrowe@gmail.com>
2018-06-24 13:13:43 -0500

    removes bar chart

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import LineChart from '../../elements/line-chart/index';
-import BarChart from '../../elements/bar-chart/index';
 
 import { ChartWrapper } from './styled';
 
 class Chart extends Component {
   render() {
-    const { finalAmount, graphData, retireAmt } = this.props;
+    const { graphData } = this.props;
     return (
       <ChartWrapper>
         <LineChart graphData={graphData}/>
-        <BarChart finalAmount={finalAmount} retireAmt={retireAmt}/>
       </ChartWrapper>
     );
   }
