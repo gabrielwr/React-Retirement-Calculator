03041a6896d325c197f3c322b8bb731da91ab425
Gabriel <Gabriel.wrowe@gmail.com>
2018-06-24 13:12:58 -0500

    adds polish to navbar

--- src/elements/navbar/styled.js
+++ src/elements/navbar/styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Link } from 'react-router';
 
 export const Navbar = styled.nav`
-  background-image: linear-gradient(-90deg, #3023AE 0%, #53A0FD 77%, #59A5F2 88%, #82C5AB 95%);
+  background-image: linear-gradient(90deg, #3023AE 0%, #53A0FD 77%, #59A5F2 88%, #82C5AB 95%);
   box-shadow: 0 2px 4px 0 rgba(0,0,0,0.50);
   height: 10%;
 `;
@@ -19,11 +19,11 @@ export const NavbarList = styled.ul`
 export const NavbarLink = styled.li`
   list-style-type: none;
   display: block;
-
 `;
 
 export const StyledLink = styled(Link)`
   color: white;
   font-size: 24px;
   text-shadow: 0 2px 4px rgba(0,0,0,0.50);
+  text-decoration: none;
 `;
