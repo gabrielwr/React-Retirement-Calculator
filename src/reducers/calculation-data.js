a83d02b5564d3380de7c7fd900d3555485becbb5
Gabriel <Gabriel.wrowe@gmail.com>
2018-07-10 15:13:43 -0500

    removes all reducers and adds calculation-data reducer

--- src/reducers/calculation-data.js
+++ src/reducers/calculation-data.js
@@ -7,27 +7,21 @@ import * as R from 'ramda';
 
 */
 export const actionTypes = {
-  SET_STARTING_AGE: 'CALCULATION_DATA_SET_STARTING_AGE',
-  SET_STARTING_SAVINGS: 'CALCULATION_DATA_SET_STARTING_SAVINGS',
   SET_FINAL_SAVINGS: 'CALCULATION_DATA_SET_FINAL_SAVINGS',
   SET_GRAPH_DATA: 'CALCULATION_DATA_SET_GRAPH_DATA',
   SET_INVESTMENT_RETURN_RATE: 'CALCULATION_DATA_SET_INVESTMENT_RETURN_RATE',
   SET_LIFE_EXPECTANCY: 'CALCULATION_DATA_SET_LIFE_EXPECTANCY',
   SET_RETIRE_AGE: 'CALCULATION_DATA_SET_RETIRE_AGE',
-  SET_RETIRE_SAVINGS: 'CALCULATION_DATA_SET_RETIRE_SAVINGS',
+  SET_RETIRE_AMT: 'CALCULATION_DATA_SET_RETIRE_AMT',
   SET_RETIRE_SPENDING: 'CALCULATION_DATA_SET_RETIRE_SPENDING',
   SET_SALARY: 'CALCULATION_DATA_SET_SALARY',
   SET_SALARY_INCREASE: 'CALCULATION_DATA_SET_SALARY_INCREASE',
-  SET_SAVINGS: 'CALCULATION_DATA_SET_SALARY_INCREASE',
+  SET_SAVINGS_RATE: 'CALCULATION_DATA_SET_SAVINGS_RATE',
+  SET_STARTING_AGE: 'CALCULATION_DATA_SET_STARTING_AGE',
+  SET_STARTING_SAVINGS: 'CALCULATION_DATA_SET_STARTING_SAVINGS',
 };
 
 export const actions = {
-  setCurrentAge(age) {
-    return { type: actionTypes.SET_STARTING_AGE, age };
-  },
-  setCurrentSavings(savings) {
-    return { type: actionTypes.SET_STARTING_SAVINGS, savings };
-  },
   setFinalSavings(savings) {
     return { type: actionTypes.SET_FINAL_SAVINGS, savings };
   },
@@ -40,11 +34,33 @@ export const actions = {
   setLifeExpectancy(lifeExpectancy) {
     return { type: actionTypes.SET_LIFE_EXPECTANCY, lifeExpectancy };
   },
+  setRetireAge(retireAge) {
+    return { type: actionTypes.SET_RETIRE_AGE, retireAge };
+  },
+  setRetireAmt(retireAmt) {
+    return { type: actionTypes.SET_RETIRE_AMT, retireAmt };
+  },
+  setRetireSpending(retireSpending) {
+    return { type: actionTypes.SET_RETIRE_SPENDING, retireSpending };
+  },
+  setSalary(salary) {
+    return { type: actionTypes.SET_SALARY, salary };
+  },
+  setSalaryIncrease(salaryIncrease) {
+    return { type: actionTypes.SET_SALARY, salaryIncrease };
+  },
+  setSalaryIncrease(savingsRate) {
+    return { type: actionTypes.SET_SAVINGS_RATE, savingsRate };
+  },
+  setStartingAge(age) {
+    return { type: actionTypes.SET_STARTING_AGE, age };
+  },
+  setStartingAge(savings) {
+    return { type: actionTypes.SET_STARTING_SAVINGS, savings };
+  },
 };
 
 export const INITIAL_STATE = {
-  startingAge: '26',
-  startingSavings: '0',
   finalSavings: '0',
   graphData: [],
   investmentReturnRate: '4',
@@ -54,21 +70,15 @@ export const INITIAL_STATE = {
   retireSpending: '40000',
   salary: '50000',
   salaryIncrease: '3',
-  savings: '1',
+  savingsRate: '10',
+  startingAge: '26',
+  startingSavings: '0',
 };
 
 export default reducer = (_state = INITIAL_STATE, action) => {
   let state = Object.assign({}, _state);
 
   switch(action.type) {
-    case actionTypes.SET_STARTING_AGE:
-      state = R.assoc('startingAge', action.age)(state);
-      break;
-
-    case actionTypes.SET_STARTING_SAVINGS:
-      state = R.assoc('startingSavings', action.savings)(state);
-      break;
-
     case actionTypes.SET_FINAL_SAVINGS:
       state = R.assoc('finalSavings', action.savings)(state);
       break;
@@ -83,6 +93,38 @@ export default reducer = (_state = INITIAL_STATE, action) => {
 
     case actionTypes.SET_LIFE_EXPECTANCY:
       state = R.assoc('lifeExpectancy', action.lifeExpectancy)(state);
+      break;
+
+    case actionTypes.SET_RETIRE_AGE:
+      state = R.assoc('retireAge', action.retireAge)(state);
+      break;
+
+    case actionTypes.SET_RETIRE_AMT:
+      state = R.assoc('retireAmt', action.retireAmt)(state);
+      break;
+
+    case actionTypes.SET_RETIRE_SPENDING:
+      state = R.assoc('retireSpending', action.retireSpending)(state);
+      break;
+
+    case actionTypes.SET_SALARY:
+      state = R.assoc('salary', action.salary)(state);
+      break;
+
+    case actionTypes.SET_SALARY_INCREASE:
+      state = R.assoc('salaryIncrease', action.salaryIncrease)(state);
+      break;
+
+    case actionTypes.SET_SAVINGS_RATE:
+      state = R.assoc('savingsRate', action.savingsRate)(state);
+      break;
+
+    case actionTypes.SET_STARTING_AGE:
+      state = R.assoc('startingAge', action.age)(state);
+      break;
+
+    case actionTypes.SET_STARTING_SAVINGS:
+      state = R.assoc('startingSavings', action.savings)(state);
       break;
 
     default:
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,31 +1,9 @@
 import { combineReducers } from 'redux';
 
 import calculationData from './calculation-data';
-import currentSavings from './currentSavings';
-import finalAmount from './finalAmt';
-import graphData from './graphData';
-import lifespan from './lifespan';
-import marketReturn from './marketReturn';
-import retireAge from './retireAge';
-import retireAmt from './retireAmt';
-import retireSpending from './retireSpending';
-import salary from './salary';
-import salaryIncrease from './salaryIncrease';
-import savings from './savings';
 
 const rootReducer = combineReducers({
   calculationData,
-  currentSavings,
-  finalAmount,
-  graphData,
-  lifespan,
-  marketReturn,
-  retireAge,
-  retireAmt,
-  retireSpending,
-  salary,
-  salaryIncrease,
-  savings
-})
+});
 
 export default rootReducer;
