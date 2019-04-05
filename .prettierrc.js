f9dbbac494ecf45724f0687e10bb3849126f7c4d
Gabriel <gabriel.wrowe@gmail.com>
2019-04-04 20:07:57 -0500

    feat: adds rudimentary travis yaml file; adds react-test-rendered for basic snapshot; adds first pass at proptypes for the component

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.expors = {
+  // Prettier
+  "prettier.printWidth": 80,
+  "prettier.singleQuote": true,
+  "prettier.trailingComma": "none",
+  "prettier.bracketSpacing": true,
+  "prettier.jsxBracketSameLine": false,
+  "prettier.semi": true,
+  "prettier.useTabs": false,
+  "prettier.tabWidth": 2,
+}
